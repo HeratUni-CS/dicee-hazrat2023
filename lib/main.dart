0aebc54867313ccc7458ecbc8bce8390f4c02c6a
ghulam hazrat <hazrat.sultani@gmail.com>
2023-05-02 12:18:34 -0700

    this is dice project that created in dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -9,15 +10,56 @@ void main() {
           backgroundColor: Colors.red,
           title: Text('Dice'),
         ),
-        body: MyApp(),
+        body: DicePage(),
       ),
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDicenumber = 1;
+  int rightDicenumber = 1;
+
+  void DiceChangeFace(){
+    setState(() {
+      leftDicenumber = Random().nextInt(6) + 1;
+      rightDicenumber = Random().nextInt(6) + 1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FloatingActionButton(
+                onPressed: (){
+                  DiceChangeFace();
+                  print("left button has pressed");
+                },
+                child: Image.asset("images/dice$leftDicenumber.png"),
+              ),
+            ),
+
+            Expanded(
+              child: FloatingActionButton(
+                onPressed: (){
+                  DiceChangeFace();
+                  print("right button has pressed");
+                },
+                child: Image.asset("images/dice$rightDicenumber.png"),
+              ),
+            ),
+          ]
+      ),
+    );
   }
 }
+
